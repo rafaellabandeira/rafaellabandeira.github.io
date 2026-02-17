36152303c653629ef06b0a24403e77753a613b50
rafaellabandeira <rafabansko@gmail.com>
2026-02-17 17:20:37 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 // server/server.js
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { sincronizarReservas } from "./BookingSync.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// ===== Sincronizar reservas al iniciar el servidor =====
+sincronizarReservas(); // actualiza reservas.json al arrancar
 
 // Endpoint para devolver reservas de Campanilla
 app.get("/reservas", async (req, res) => {
@@ -16,3 +20,5 @@ app.get("/reservas", async (req, res) => {
 // Puerto dinámico para Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
+
+
